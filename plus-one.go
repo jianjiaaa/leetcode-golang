fb55efc0e7b313c53ac50f1a51c9305e49ebf28a
jianjiaaa <tanj0235@e.ntu.edu.sg>
2021-02-01 12:12:14 +0800

    Add #66 Plus One solution

---
+++ plus-one.go
@@ -0,0 +1,12 @@
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	number := make([]int, len(digits)+1)
+	number[0] = 1
+	return number
+}
